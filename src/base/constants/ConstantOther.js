2747819d4660e2c60fd9fd58f221fdbb2cad14f4
Vitaliy Zarubin <keygenqt@gmail.com>
2023-01-25 17:59:20 +0300

    Up year

--- src/base/constants/ConstantOther.js
+++ src/base/constants/ConstantOther.js
@@ -4,7 +4,7 @@
 export const ConstantOther = {
     nickname: '@keygenqt',
     email: 'dev@keygenqt.com',
-    copy: '© 2022 KeyGenQt',
+    copy: '© 2023 KeyGenQt',
     languages: {
         ru: 'ru',
         en: 'en'
